92727ef9e28796c3d428a6857bb4d81af447fd8a
Kaique Gazola <me@kaique.dev>
2021-09-28 08:42:20 -0300

    refactor pages

---
+++ lib/connect/connect_page.dart
@@ -0,0 +1,193 @@
+import 'dart:async';
+
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+class ConnectPage extends StatefulWidget {
+  const ConnectPage({Key? key}) : super(key: key);
+
+  @override
+  _ConnectPageState createState() => _ConnectPageState();
+}
+
+class _ConnectPageState extends State<ConnectPage> {
+  BluetoothDevice? selectedDevice;
+  StreamController<String> status = StreamController<String>();
+  String? lastStatus;
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Connect'),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        flutterBlue.startScan(
+                            timeout: const Duration(seconds: 30));
+                        status.add('Scanning...');
+                      },
+                      child: const Text('Start Scan'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        flutterBlue.stopScan();
+                        status.add('Idle.');
+                      },
+                      child: const Text('Stop Scan'),
+                    ),
+                  ],
+                ),
+                StreamBuilder<List<ScanResult>>(
+                  stream: flutterBlue.scanResults,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<ScanResult>> snapshot) {
+                    if (snapshot.data == null) {
+                      return Container();
+                    }
+                    if (snapshot.data!.isNotEmpty) {
+                      return Wrap(
+                        alignment: WrapAlignment.center,
+                        children: <Widget>[
+                          ...snapshot.data!
+                              .map(
+                                (ScanResult e) => GestureDetector(
+                                  onTap: () async {
+                                    if (await deviceConnected(e.device)) {
+                                      setState(() {
+                                        selectedDevice = e.device;
+                                      });
+                                    } else {
+                                      connectDevice(e.device);
+                                    }
+                                  },
+                                  child: _avatar(
+                                    e.device,
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ],
+                      );
+                    } else {
+                      return const Text('Nenhum device encontrado.');
+                    }
+                  },
+                ),
+                StreamBuilder<String>(
+                  stream: status.stream,
+                  builder:
+                      (BuildContext context, AsyncSnapshot<String> snapshot) {
+                    return Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Text(
+                        snapshot.data ?? 'Idle.',
+                        style: const TextStyle(
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateColor.resolveWith(
+                      (Set<MaterialState> states) => Colors.black,
+                    ),
+                    backgroundColor: MaterialStateColor.resolveWith(
+                      (Set<MaterialState> states) => Colors.tealAccent,
+                    ),
+                  ),
+                  onPressed: selectedDevice != null ? () {} : null,
+                  child: const Text('Send data'),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget _avatar(BluetoothDevice device) {
+    return FutureBuilder<bool>(
+        future: deviceConnected(device),
+        initialData: false,
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          return AvatarGlow(
+            glowColor: snapshot.data! ? Colors.blue : Colors.grey,
+            endRadius: 60.0,
+            duration: const Duration(milliseconds: 2000),
+            repeat: true,
+            showTwoGlows: true,
+            repeatPauseDuration: const Duration(milliseconds: 100),
+            child: Material(
+              elevation: 5.0,
+              shape: const CircleBorder(),
+              child: CircleAvatar(
+                backgroundColor: Colors.grey[100],
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Text(
+                      deviceName(
+                            device,
+                          ) +
+                          (device == selectedDevice ? '\n(selected)' : ''),
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: device == selectedDevice
+                            ? Colors.deepPurpleAccent
+                            : Colors.blue,
+                        height: 1.5,
+                      ),
+                    ),
+                  ),
+                ),
+                radius: 40.0,
+              ),
+            ),
+          );
+        });
+  }
+
+  Future<void> connectDevice(BluetoothDevice device) async {
+    try {
+      final String name = deviceName(device);
+      if (!await deviceConnected(device)) {
+        status.add('Connecting to $name...');
+        return device.connect().then((void v) async {
+          final bool connected = await deviceConnected(device);
+          status.add(
+            connected ? 'Connected to $name.' : 'Failed to connect in $name.',
+          );
+        });
+      }
+      status.add('Already connected to $name');
+    } catch (e) {
+      status.add('error in connect device: $e');
+    }
+  }
+
+  String deviceName(BluetoothDevice device) {
+    return device.name.isNotEmpty ? device.name : device.id.id;
+  }
+
+  Future<bool> deviceConnected(BluetoothDevice device) async {
+    final List<BluetoothDevice> list = await flutterBlue.connectedDevices;
+    return list.contains(device);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:async';
-
-import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
+import 'package:poc_bluetooth/connect/connect_page.dart';
+import 'package:poc_bluetooth/host/host_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,164 +17,47 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const ChoosePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  StreamController<String> status = StreamController<String>();
-  String? lastStatus;
-  FlutterBlue flutterBlue = FlutterBlue.instance;
+class ChoosePage extends StatelessWidget {
+  const ChoosePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-      child: SingleChildScrollView(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    flutterBlue.startScan(timeout: const Duration(seconds: 30));
-                    status.add('Scanning...');
-                  },
-                  child: const Text('Start Scan'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    flutterBlue.stopScan();
-                    status.add('Idle.');
-                  },
-                  child: const Text('Stop Scan'),
-                ),
-              ],
-            ),
-            StreamBuilder<List<ScanResult>>(
-              stream: flutterBlue.scanResults,
-              builder: (BuildContext context,
-                  AsyncSnapshot<List<ScanResult>> snapshot) {
-                if (snapshot.data == null) {
-                  return Container();
-                }
-                if (snapshot.data!.isNotEmpty) {
-                  return Wrap(
-                    alignment: WrapAlignment.center,
-                    children: <Widget>[
-                      ...snapshot.data!
-                          .map(
-                            (ScanResult e) => GestureDetector(
-                              onTap: () {
-                                connectDevice(e.device);
-                              },
-                              child: _avatar(
-                                e.device,
-                              ),
-                            ),
-                          )
-                          .toList(),
-                    ],
-                  );
-                } else {
-                  return const Text('Nenhum device encontrado.');
-                }
-              },
-            ),
-            StreamBuilder<String>(
-              stream: status.stream,
-              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                return Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Text(
-                    snapshot.data ?? 'Idle.',
-                    style: const TextStyle(
-                      color: Colors.blueAccent,
+    return SafeArea(
+      child: Container(
+        color: Colors.grey[100],
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.of(context).push<void>(
+                    MaterialPageRoute<void>(
+                      builder: (BuildContext context) => const ConnectPage(),
                     ),
-                  ),
-                );
-              },
-            )
-          ],
+                  );
+                },
+                child: const Text('Connect'),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.of(context).push<void>(
+                    MaterialPageRoute<void>(
+                      builder: (BuildContext context) => const HostPage(),
+                    ),
+                  );
+                },
+                child: const Text('Host'),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
-  }
-
-  Widget _avatar(BluetoothDevice device) {
-    return FutureBuilder<bool>(
-        future: deviceConnected(device),
-        initialData: false,
-        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-          return AvatarGlow(
-            glowColor: snapshot.data! ? Colors.blue : Colors.grey,
-            endRadius: 60.0,
-            duration: const Duration(milliseconds: 2000),
-            repeat: true,
-            showTwoGlows: true,
-            repeatPauseDuration: const Duration(milliseconds: 100),
-            child: Material(
-              elevation: 8.0,
-              shape: const CircleBorder(),
-              child: CircleAvatar(
-                backgroundColor: Colors.grey[100],
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: FittedBox(
-                    fit: BoxFit.cover,
-                    child: Text(
-                      deviceName(
-                        device,
-                      ),
-                      maxLines: 2,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-                radius: 40.0,
-              ),
-            ),
-          );
-        });
-  }
-
-  Future<void> connectDevice(BluetoothDevice device) async {
-    try {
-      final String name = deviceName(device);
-      if (!await deviceConnected(device)) {
-        status.add('Connecting to $name...');
-        return device.connect().then((void v) async {
-          final bool connected = await deviceConnected(device);
-          status.add(
-            connected ? 'Connected to $name.' : 'Failed to connect in $name.',
-          );
-        });
-      }
-      status.add('Already connected to $name');
-    } catch (e) {
-      status.add('error in connect device: $e');
-    }
-  }
-
-  String deviceName(BluetoothDevice device) {
-    return device.name.isNotEmpty ? device.name : device.id.id;
-  }
-
-  Future<bool> deviceConnected(BluetoothDevice device) async {
-    final List<BluetoothDevice> list = await flutterBlue.connectedDevices;
-    return list.contains(device);
+    );
   }
 }
